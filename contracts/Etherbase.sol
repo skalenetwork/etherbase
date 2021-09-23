4a1fd534f54541f716f0c6d2360041ca22becda8
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-09-23 16:22:35 +0300

    SKALE-4508 Setup project

---
+++ contracts/Etherbase.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+/**
+ *   Etherbase.sol - Etherbase
+ *   Copyright (C) 2021-Present SKALE Labs
+ *   @author Dmytro Stebaiev
+ *
+ *   Etherbase is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Etherbase is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with Etherbase.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+pragma solidity ^0.8.7;
+
+contract Etherbase {
+
+}
