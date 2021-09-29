747e2d22481ccf54fd4b76870e38e04792370614
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-09-29 15:43:48 +0300

    SKALE-4508 Fix error in file path

--- predeployed/scripts/build_package.sh
+++ predeployed/scripts/build_package.sh
@@ -4,6 +4,6 @@ set -e
 
 cd "$(dirname "$0")/.."
 ./scripts/generate_package_version.py > version.txt
-ARTIFACTS_DIR="../src/etherbase_predeployed/artifacts/"
-cp -v "../../artifacts/contracts/Etherbase.sol/Etherbase.json" "$ARTIFACTS_DIR"
+ARTIFACTS_DIR="src/etherbase_predeployed/artifacts/"
+cp -v "../artifacts/contracts/Etherbase.sol/Etherbase.json" "$ARTIFACTS_DIR"
 python3 -m build
