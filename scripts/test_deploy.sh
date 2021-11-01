c601157dd70a161614ed85f684b8889067bdd648
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-11-01 19:05:22 +0200

    SKALE-4508 Change test deploy script

--- scripts/test_deploy.sh
+++ scripts/test_deploy.sh
@@ -10,3 +10,14 @@ MODE=UPGRADEABLE npx hardhat run migrations/deploy.ts
 
 kill $GANACHE_PID
 tail --pid=$GANACHE_PID -f /dev/null
+
+while true
+do
+    netstat -t -u -l -p -n | grep 8545
+    if [ $? -ne 0 ]
+    then
+        break
+    fi
+    echo "Port 8545 is busy"
+    sleep 1
+done
