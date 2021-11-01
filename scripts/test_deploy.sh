0defc07f1dc8ee348cedaa1fe8128f43d9b2d203
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-11-01 17:18:16 +0200

    SKALE-4508 Modify deploy check

--- scripts/test_deploy.sh
+++ scripts/test_deploy.sh
@@ -4,6 +4,9 @@ set -e
 
 npx ganache-cli --gasLimit 8000000 --quiet &
 GANACHE_PID=$!
+
 MODE=FIXED npx hardhat run migrations/deploy.ts
 MODE=UPGRADEABLE npx hardhat run migrations/deploy.ts
+
 kill $GANACHE_PID
+tail --pid=$GANACHE_PID -f /dev/null
