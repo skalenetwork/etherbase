9584357803be0d9469df993c9917f0ebf638d3f8
Dmytro Stebaiev <dmytro@skalelabs.com>
2022-01-28 16:27:20 +0200

    Update solc

--- contracts/EtherbaseUpgradeable.sol
+++ contracts/EtherbaseUpgradeable.sol
@@ -19,7 +19,7 @@
  *   along with Etherbase.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
 
