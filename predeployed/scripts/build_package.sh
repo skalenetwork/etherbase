33ef09856a7c47fdfad6ae2b5cd84d13d1edbfcd
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-07-01 12:25:22 +0300

    Update scripts to use ArtifactsHandler

--- predeployed/scripts/build_package.sh
+++ predeployed/scripts/build_package.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "$(dirname "$0")/.."
 ./scripts/generate_package_version.py > version.txt
-python3 $SCRIPT_DIR/generate_artifacts.py
+python3 $SCRIPT_DIR/prepare_artifacts.py
 python3 -m build
